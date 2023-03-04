07d918b756261eb3b2a562103207133d4a5eea2b
刘俊余 <tangyu@MacBook-Pro-4.local>
2023-03-04 13:47:28 -0600

    update p1-1

--- hw4/hw4_1/p1-1.asm
+++ hw4/hw4_1/p1-1.asm
@@ -1,1 +1,20 @@
 // Write your assembly program for Problem 1 (a) #1 here.
+
+// Test for ANDN instruction
+lbi r1, 0xff        // expected r1 = 0xffff
+lbi r2, 0x0E        // expected r2 = 0x000E
+lbi r0, U.Here
+slbi r0, L.Here     // r0 contains address of ".Here"
+st r2, r0, 0        // .Here should contain the value 0x000E
+ld r3, r0, 0        // r3 = 0x000E
+andn r4, r1, r2     // expected r4 = 0xfff1
+halt
+halt
+halt
+halt
+halt
+halt
+halt
+halt
+.Here:
+halt
