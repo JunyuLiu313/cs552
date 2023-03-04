911d47eb9b80098a022dfc88fb3ef34028b860c1
刘俊余 <tangyu@MacBook-Pro-4.local>
2023-03-04 15:08:37 -0600

    update p2-2

--- hw4/hw4_2/p2-2.asm
+++ hw4/hw4_2/p2-2.asm
@@ -1,1 +1,11 @@
 // Write your assembly program for Problem 2 (a) #2 here.
+.Start:
+lbi r2, 0x00		// r2 = 0
+lbi r3, 0x14		// r3 = 20
+
+.Loop:
+addi r2, r2, 0x01	// r2 += 1
+sub r4, r2, r3		// r4 = r3 - r2
+bnez r4, .Loop
+andni r2, r2, 0xff	// r2 = 0
+halt
