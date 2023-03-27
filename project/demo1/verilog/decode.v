ad566eea767e4177f3c2649d2eb915ca1a359604
JunyuLiu313 <109556195+JunyuLiu313@users.noreply.github.com>
2023-03-26 22:14:46 -0500

    Add control signals for MEM and WB stage

--- project/demo1/verilog/decode.v
+++ project/demo1/verilog/decode.v
@@ -1,3 +1,4 @@
+
 /*
    CS/ECE 552 Spring '22
   
@@ -5,16 +6,22 @@
    Description     : This is the module for the overall decode stage of the processor.
 */
 `default_nettype none
-module decode (INSTR, Opcode, imm, RsData, RtData, AluOP, clk, rst);
+module decode (INSTR, Opcode, imm, RsData, RtData, AluOP, clk, rst, addr, DCaddr, memWrite, memRead, savePC);
 
 	// TODO: Your code here
-	input wire [15:0] INSTR;
+	input wire [15:0] INSTR, addr;
 	input wire clk, rst;
    
-	output wire [15:0]	RsData, RtData;
+	output wire [15:0]	RsData, RtData, DCaddr;
 	output wire [4:0] 	Opcode;
 	output wire [16:0] 	imm;
 	output wire [1:0]	AluOP;
+
+	output wire memWrite, memRead;		// control signals for MEM stage
+	output wire savePC;			// true if R7 <- PC + 2
+
+	// pass the pc address
+	assign DCaddr = addr;
 
 
 	//	control singals
@@ -26,8 +33,8 @@ module decode (INSTR, Opcode, imm, RsData, RtData, AluOP, clk, rst);
 	assign nop = (!(INSTR[15]|INSTR[14]|INSTR[13]|INSTR[12]))&INSTR[11];
 	assign Rd_i = (INSTR[15]&~INSTR[14]&~INSTR[13]&INSTR[12]&INSTR[11]) ? Rs : Rd;
 	assign writeRegSel = ~INSTR[15] & ~INSTR[14] & INSTR[13] & INSTR[12] ? 3'b111 :	Rd_i; 
-
-	 
+	
+	// EX control signals 
 	assign z_ext_1 = ~INSTR[15]&INSTR[14]&~INSTR[13]&INSTR[12];
 	assign z_ext_2 =  INSTR[15]&~INSTR[14]&~INSTR[13]&INSTR[12];
 
@@ -41,6 +48,13 @@ module decode (INSTR, Opcode, imm, RsData, RtData, AluOP, clk, rst);
 	assign i_2 		= z_ext_2 ? {{8{1'b0}}, INSTR[7:0]}	: {{8{INSTR[7]}},INSTR[7:0]};
 	assign imm_i 	= ((INSTR[15]&~INSTR[14])|(~INSTR[15]&INSTR[14]&~INSTR[13])|(~INSTR[15]&~INSTR[14]&INSTR[13]&~INSTR[12]&INSTR[11])|(~INSTR[15]&~INSTR[14]&INSTR[13]&INSTR[12]&INSTR[11])) ? i_1 : i_2;
 	assign imm 		= (~INSTR[15]&~INSTR[14]&INSTR[13]) ? {{5{INSTR[10]}}, INSTR[10:0]} : imm_i;
+
+	// control signals for Memory stage
+	assign memWrite = (INSTR[15]&~INSTR[14]&~INSTR[13]&~INSTR[12]&~INSTR[11]) | (INSTR[15]&~INSTR[14]&~INSTR[13]&INSTR[12]&INSTR[11]);
+	assign memRead = (INSTR[15]&~INSTR[14]&~INSTR[13]&~INSTR[12]&INSTR[11]);
+
+	// WB control signals
+	assign savePC = (~INSTR[15]&~INSTR[14]&INSTR[13]&INSTR[12]);
 
 	rf_bypass RF (
                   .read1OutData(RsData), .read2OutData(RtData), .err(exc),
