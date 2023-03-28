60909148b02f564dda8801f1bc2fa49a94fa4a1a
bhavitsunkara <79113358+bhavitsunkara@users.noreply.github.com>
2023-03-28 06:49:13 -0500

    Add files via upload
    added logic for handing jump, branches, and exception, but still need to determine if the branch is taken or not

---
+++ project/demo1/verilog/execute1.v
@@ -0,0 +1,33 @@
+/*
+   CS/ECE 552 Spring '22
+  
+   Filename        : execute.v
+   Description     : This is the overall module for the execute stage of the processor.
+*/
+`default_nettype none
+module execute (
+	// inputs
+	OPCODE, Rs, Rt, Imm, PC, FUNC, halt, nop, JR, jump, SIIC, RTI,
+	// outputs
+	nxtPC, Result, err);
+
+   // TODO: Your code here
+	input wire [4:0] OPCODE;
+	input wire [15:0] Rs, Rt, Imm, PC;
+	input wire [1:0] FUNC;
+	input wire halt, nop, SIIC, RTI, JR, jump;
+	
+	output wire [15:0] Result, nxtPC;
+	output wire err;
+	
+	//	logic for updating PC and hadling exception
+	wire [15:0] incPC, incPC_i, updPC, Imm_i;
+	wire branch;
+	cla16b PC_INC( .sum(incPC_i), .cOut(), .inA(PC), .inB(16'b10), .cIn(1'b0));
+	assign incPC = JR | RTI ? Rs : incPC_i;
+	assign Imm_i = jump | branch ? Imm : 16'b0; 
+	cla16b PC_UPD(.sum(updPC), .cOut(), .inA(incPC), .inB(Imm_i));
+	assign nxtPC = SIIC ? 16'b0000000000000010 : updPC;    
+   
+endmodule
+`default_nettype wire
