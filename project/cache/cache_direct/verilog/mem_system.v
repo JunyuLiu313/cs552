174d5a84648ba2aa08a1c6c0d276e705fd5a72ab
JunyuLiu313 <109556195+JunyuLiu313@users.noreply.github.com>
2023-04-28 20:27:41 -0500

    fsm modified to single cycle, and fixed err sig
    Success: 1764 hits

--- project/cache/cache_direct/verilog/mem_system.v
+++ project/cache/cache_direct/verilog/mem_system.v
@@ -24,6 +24,9 @@ module mem_system(/*AUTOARG*/
    output reg        CacheHit;
    output reg        err;
 
+/////////////////////////////////////////////////////////////////////
+//////////////////////   Internal Signals       /////////////////////
+/////////////////////////////////////////////////////////////////////
    // cache signals
    // inputs 
    reg        enable;
@@ -67,37 +70,46 @@ module mem_system(/*AUTOARG*/
 
    // four-bank mem
    // inputs
-   wire [15:0] data_in_mem_wire, Addr_mem_wire;
-   wire wr_mem_wire, rd_mem_wire;
+   wire [15:0]    data_in_mem_wire, Addr_mem_wire;
+   wire           wr_mem_wire, rd_mem_wire;
    // outputs
    wire [15:0]    data_out_mem_wire;
    wire [3:0]     busy_wire;
    wire           stall_mem_wire;
 
+   // error signals from cache, 4-bank mem, and FSM
+   wire err_cache, err_mem;
+   reg err_allign;
 
-   
+   // state signals
+   reg [3:0] state, nxt_state;
+   wire [3:0] state_wire, nxt_state_wire;
+
+   // flipflop the inputs
+   wire [15:0] Addr_ff, DataIn_ff;
+   wire Rd_ff, Wr_ff;
+
+   // misc signals
+   reg [4:0] tag_ff_d;
+
+/////////////////////////////////////////////////////////////////////
+//////////////////////    Assign statements     /////////////////////
+/////////////////////////////////////////////////////////////////////
+
    // assign all the reg signals to their corresponding wire signals
-   assign enable_wire = enable;
-   assign index_wire = index;
-   assign offset_wire = offset;
-   assign comp_wire = comp;
-   assign write_wire = write;
-   assign tag_in_wire = tag_in;
-   assign data_in_wire = data_in ;
-   assign valid_in_wire = valid_in;
-   //assign hit_wire = hit;
-   // assign dirty_wire = dirty;
-   // assign tag_out_wire = tag_out;
-   // assign data_out_wire = data_out;
-   // assign valid_wire = valid;
+   assign enable_wire      = enable;
+   assign index_wire       = index;
+   assign offset_wire      = offset;
+   assign comp_wire        = comp;
+   assign write_wire       = write;
+   assign tag_in_wire      = tag_in;
+   assign data_in_wire     = data_in ;
+   assign valid_in_wire    = valid_in;
    assign data_in_mem_wire = data_in_mem;
-   assign Addr_mem_wire = Addr_mem;
-   assign wr_mem_wire = wr_mem;
-   assign rd_mem_wire = rd_mem;
-   // assign data_out_mem_wire = data_out_mem;
-   // assign busy_wire = busy;
-   
-
+   assign Addr_mem_wire    = Addr_mem;
+   assign wr_mem_wire      = wr_mem;
+   assign rd_mem_wire      = rd_mem;
+   assign nxt_state_wire   = nxt_state; 
    // FSM states
    localparam [3:0]
    IDLE     =  4'b0000,
@@ -116,8 +128,7 @@ module mem_system(/*AUTOARG*/
    COMP2    =  4'b1101,
    DONE     =  4'b1110;
 
-   // error signals from cache and 4-ban mem
-   wire err_cache, err_mem;
+
                                
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
@@ -156,84 +167,79 @@ module mem_system(/*AUTOARG*/
                      .wr                (wr_mem_wire),
                      .rd                (rd_mem_wire));
    
-   reg [3:0] state, nxt_state;
-   wire [3:0] state_wire, nxt_state_wire;
-   assign nxt_state_wire   = nxt_state;  
-   dff STATE [3:0] (.q(state_wire), .d(nxt_state_wire), .clk(clk), .rst(rst));
 
-   // flipflop the inputs
-   wire [15:0] Addr_ff, DataIn_ff;
-   wire Rd_ff, Wr_ff;
+/////////////////////////////////////////////////////////////////////
+//////////////////////  DFF Module Declaration  /////////////////////
+/////////////////////////////////////////////////////////////////////
+
+   dff STATE [3:0] (.q(state_wire), .d(nxt_state_wire), .clk(clk), .rst(rst));
    dff_enable ADDR [15:0] (.q(Addr_ff), .d(Addr), .clk(clk), .rst(rst), .enable(~Stall));  
    dff_enable DATAIN [15:0] (.q(DataIn_ff), .d(DataIn), .clk(clk), .rst(rst), .enable(~Stall));
    dff_enable READ (.q(Rd_ff), .d(Rd), .clk(clk), .rst(rst), .enable(~Stall)); 
-   dff_enable WRITE (.q(Wr_ff), .d(Wr), .clk(clk), .rst(rst), .enable(~Stall));     
-
-   reg err_allign;
-   reg [4:0] tag_ff_d;
-   // wire [4:0] tag_wire_d, tag_wire_q;
-   // assign tag_wire_d = tag_ff_d;
-   // dff TAG[4:0] (.q(tag_wire_q), .d(tag_wire_d), .clk(clk), .rst(rst));
+   dff_enable WRITE (.q(Wr_ff), .d(Wr), .clk(clk), .rst(rst), .enable(~Stall));
+     
+/////////////////////////////////////////////////////////////////////
+//////////////////////  FSM comb and seq logic  /////////////////////
+/////////////////////////////////////////////////////////////////////
 
 always @*
    begin
    
    // defualt logic
-   // hit      = hit_wire;
-   dirty    = dirty_wire;
-   // tag_out  = tag_out_wire;   
-   data_out = data_out_wire;   
-   // valid    = valid_wire;
-
-   state    = state_wire;
-   err_allign = 0;
-   write    = 1'b0;
-
-   data_in  = DataIn;
-   data_out_mem = data_out_mem_wire;
-   busy     = busy_wire;
-   enable   = Rd_ff | Wr_ff;
+   // 4 bank mem signals
+   data_out_mem   = data_out_mem_wire;
+   busy           = busy_wire;
+   
+   // mem system level signals
    Done     = 1'b0;
    Stall    = 1'b1;
-   DataOut     = data_out_wire;
-
-   // dirty    = 1'b0;
    CacheHit = 1'b0;
+   DataOut  = data_out_wire;
+   data_in  = DataIn;
 
    // cache inputs
+   comp        = 0;
    tag_in      = Addr_ff[15:11];
    index       = Addr_ff[10:3];
    offset      = Addr_ff[2:0];
-   valid_in    = Wr_ff | Rd_ff;
+   Stall       = 1'b1;
+   dirty       = dirty_wire;
+   data_out    = data_out_wire;   
+   enable      = 0;
+   write       = 1'b0;
 
-   nxt_state = 4'h0;
+   err         = 0;
+   err_allign  = 0;
+
+   state       = state_wire;
+   nxt_state   = 4'h0;
    
 
       case(state)
          IDLE: begin
-            valid_in    = Rd | Wr; //Rd_ff | Wr_ff;
-            // DataOut     = data_out_wire;
-            data_in_mem = DataIn; //DataIn_ff;
+
+            tag_in      = Addr[15:11];
+            index       = Addr[10:3];
+            offset      = Addr[2:0];
+            valid_in    = Rd | Wr; 
+            write       = Wr;
+            comp        = Rd | Wr;
+            enable      = Rd | Wr;
+            data_in     = Wr ? DataIn : 16'h0000;
+
+            data_in_mem = DataIn_ff;
             Addr_mem    = 16'h0;
             wr_mem      = 0;
             rd_mem      = 0;
-            comp        = hit_wire;
-            // CacheHit    = hit_wire & valid_wire;
-            // Done        = CacheHit;
-            // write       = Wr_ff;
 
-            data_in     = Wr ? DataIn : 16'h0000;
-            // DataOut     = data_out;
+            Stall       = 0;
+            CacheHit    = hit_wire & valid_in_wire;
+            Done        = CacheHit & valid_in_wire & (~dirty | ~Wr);
 
-            // only not stall in IDLE state
-            Stall       = (nxt_state != IDLE);
-
-
-            // only go to COMP1 if Rd or Wr is true
-            nxt_state   =  Addr_ff[0] ? ERR :
-                           (Rd | Wr) ? COMP1: 
-                           IDLE;
-
+            nxt_state   =  Addr[0]? ERR:
+                           (~hit_wire & dirty_wire) ? WB0:
+                           (~hit_wire & ~dirty_wire & valid_in_wire) ? ALLOC0 : 
+                           (~valid_in_wire | (hit_wire & valid_wire)) ?  IDLE : ALLOC0;
 
          end
 
@@ -243,65 +249,57 @@ always @*
             nxt_state   = IDLE;
          end
 
-         COMP1: begin
-            enable      = 1'b1;
-            comp        = hit_wire;
-            CacheHit    = hit_wire & valid_wire;
-            Done        = CacheHit;
-            write       = Wr_ff & CacheHit;
-            nxt_state   =  CacheHit ? IDLE :
-                           (~dirty_wire) ?
-                           ALLOC0 : WB0;
-         end
-
          WB0: begin
             tag_in      = tag_out_wire;
             comp        = 1'b0;
+            enable      = 1'b1;
 
-            // Addr_mem    = {{Addr_ff[15:3]}, 3'b000};
             Addr_mem    = {tag_out_wire, Addr_ff[10:3], 3'b000};
             data_in_mem = data_out;
             write       = 0;
             wr_mem      = 1'b1;
             nxt_state   = stall_mem_wire ? state : WB1;
             // read word 0 from cache
-            offset = 3'b000;
+            offset      = 3'b000;
          end
 
          WB1: begin
             comp        = 1'b0;
-            // Addr_mem    = {{Addr_ff[15:3]}, 3'b010};
+            enable      = 1'b1;
+
             Addr_mem    = {tag_out_wire, Addr_ff[10:3], 3'b010};
             data_in_mem = data_out;
             write       = 0;
             wr_mem      = 1'b1;
             nxt_state   = stall_mem_wire ? state : WB2;
             // read word 1 from cache
-            offset = 3'b010;
+            offset      = 3'b010;
          end
 
          WB2: begin
             comp        = 1'b0;
-            // Addr_mem    = {{Addr_ff[15:3]}, 3'b100};
+            enable      = 1'b1;
+
             Addr_mem    = {tag_out_wire, Addr_ff[10:3], 3'b100};
             data_in_mem = data_out;
             write       = 0;
             wr_mem      = 1'b1;
             nxt_state   = stall_mem_wire ? state : WB3;
             // read word 2 from cache
-            offset = 3'b100;
+            offset      = 3'b100;
          end
 
          WB3: begin
             comp        = 1'b0;
-            // Addr_mem    = {{Addr_ff[15:3]}, 3'b110};
+            enable      = 1'b1;
+
             Addr_mem    = {tag_out_wire, Addr_ff[10:3], 3'b110};
             data_in_mem = data_out;
             write       = 0;
             wr_mem      = 1'b1;
             nxt_state   = ALLOC0;
             // read word 3 from cache
-            offset = 3'b110;
+            offset      = 3'b110;
          end
 
          ALLOC0: begin
@@ -325,6 +323,8 @@ always @*
          ALLOC2: begin
             comp        = 1'b0;
             offset      = 3'b000;
+            enable      = 1'b1;
+
             Addr_mem    = {{Addr_ff[15:3]}, 3'b100};
             data_in     = data_out_mem; 
             write       = 1;
@@ -337,6 +337,8 @@ always @*
          ALLOC3: begin
             comp        = 1'b0;
             offset      = 3'b010;
+            enable      = 1'b1;
+
             Addr_mem    = {{Addr_ff[15:3]}, 3'b110};
             data_in     = data_out_mem; 
             write       = 1;
@@ -349,6 +351,8 @@ always @*
          ALLOC4: begin
             comp        = 1'b0;
             offset      = 3'b100;
+            enable      = 1'b1;
+
             data_in     = data_out_mem;
             write       = 1;
             valid_in    = 1;
@@ -360,43 +364,31 @@ always @*
          ALLOC5: begin
             comp        = 1'b0;
             offset      = 3'b110;
+            enable      = 1'b1;
+
             data_in     = data_out_mem;
             write       = 1;
             valid_in    = 1;
             wr_mem      = 0;
             rd_mem      = 0;
-            nxt_state   = COMP2;
-         end
-
-         COMP2: begin
-            //Done        = 1'b1;
-            write       = Wr;
-            data_in     = DataIn;
-            comp        = Wr;//1'b0;
-
-            // read the output from cache
-            enable      = 1'b1;
-
             nxt_state   = DONE;
          end
 
          // assign the data output
          DONE: begin
-            DataOut     = data_out;
             Done        = 1'b1;
             enable      = 1'b1;
-            comp        = 1'b1;
-            write       = Wr;
-
+            comp        = Wr_ff;
+            write       = Wr_ff;
             nxt_state   = IDLE;
          end
 
          default: begin 
-            nxt_state = IDLE;
+            nxt_state   = IDLE;
          end
       endcase
    end 
    
 endmodule // mem_system
 `default_nettype wire
-// DUMMY LINE FOR REV CONTROL :9:
+// DUMMY LINE FOR REV CONTROL :9:   
