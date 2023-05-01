0711882c313f864393b80678df8b1114226cbf97
JunyuLiu313 <109556195+JunyuLiu313@users.noreply.github.com>
2023-05-01 13:39:58 -0500

    implemented forwarding and stall
    need to change stall logic after we add cache to our processor

---
+++ project/demo3/verilog/hazardControl.v
@@ -0,0 +1,87 @@
+/*
+   CS/ECE 552 Spring '22
+  
+   Filename        : hazardControl.v
+   Description     : This is the module for detecting stall and forwarding in the decode stage.
+*/
+`default_nettype none
+module hazardControl(//inputs
+                     Rd_d, Rs_d, Rt_d, 
+                     Rd_x, Rd_m, 
+                     ResultSel_x, ResultSel_m, ResultSel_d,
+                     RegWrite_x, RegWrite_m,
+                     opcode_d, opcode_x,
+                     Rd_data_x, Rd_data_mStage, Rd_data_memory,
+                     Rs_data_d, Rt_data_d,
+                     //outputs 
+                     stall, forward, Rs_forward, Rt_forward);
+input wire [2:0] Rd_d, Rs_d, Rt_d;
+input wire [2:0] Rd_x, Rd_m;
+input wire [1:0] ResultSel_x, ResultSel_m, ResultSel_d;
+input wire [4:0] opcode_d, opcode_x;
+input wire RegWrite_x, RegWrite_m;
+input wire [15:0] Rd_data_x, Rd_data_mStage, Rd_data_memory, Rs_data_d, Rt_data_d;
+
+output wire stall, forward;
+output wire [15:0] Rs_forward, Rt_forward;
+
+/*
+   New for Demo3:
+      Add forwarding and change stall logic.
+      After fully implement forwarding, we only stall when accessing memory
+   
+   Forwarding:
+      if the current instruction Rs and/or Rt is also the Rd to be written in 
+      EX stage or MEM stage 
+   
+   Stall:
+      If we have a dependency Rd from the current execute stage but it's a load operation,
+      we should wait till the data is loaded from memory 
+*/
+
+wire secondReg, imm_load;
+// secondReg: 1 if there is Rt, 0 if no Rt in decode stage
+assign secondReg = ((opcode_d[4:2] == 3'b110) & opcode_d[1:0] != 2'b00) | (opcode_d[4:2] == 3'b111) | (opcode_d == 5'b10011) | (opcode_d == 5'b10000); 
+assign imm_load = (opcode_d == 5'b11000);
+                  // | (opcode_d == 5'b10010); 
+
+wire forward_Rs_x, forward_Rs_m;
+assign forward_Rs_x = ((Rs_d == Rd_x))? 1'b1 : 1'b0;
+assign forward_Rs_m = ((Rs_d == Rd_m))? 1'b1 : 1'b0;
+
+wire branchConflict;
+assign branchConflict = ((opcode_d[4:2] == 3'b011) & (Rs_d == Rd_x | Rs_d == Rd_m) ? 1'b1 : 1'b0);
+
+wire forward_Rt_x, forward_Rt_m;
+assign forward_Rt_x =  (( Rt_d == Rd_x ) & secondReg) ? 1'b1 : 1'b0;
+assign forward_Rt_m =  (( Rt_d == Rd_m ) & secondReg) ? 1'b1 : 1'b0;
+
+wire jump;
+assign jump = (opcode_d[4:1] == 3'b0011) & (Rd_x == 3'b111 | Rd_m == 3'b111);
+
+// check if the current instruction in execute stage is ld
+wire ld_ex;
+assign ld_ex = (opcode_x == 5'b10001);
+
+// comb logic for determining stall
+assign stall = ld_ex & RegWrite_x; // (opcode_d == 5'b10001) ;
+
+// forwarding part
+// Rd_data_mStage could be from memory instead of from execute
+assign forward = ((forward_Rt_x | forward_Rs_x) & (RegWrite_x) & ~ld_ex) | ((forward_Rt_m | forward_Rs_m) & (RegWrite_m));
+
+/*
+   If forwarding happen in memory stage, it could be either Rd originally from execute stage,
+   or the data from memory using load instruction
+*/
+assign Rs_forward = (RegWrite_x & forward_Rs_x) ? Rd_data_x :
+                     (RegWrite_m & forward_Rs_m & ld_ex) ? Rd_data_memory :
+                     (RegWrite_m & forward_Rs_m) ? Rd_data_mStage :
+                     Rs_data_d;
+assign Rt_forward = (RegWrite_x & forward_Rt_x) ? Rd_data_x :
+                     (RegWrite_m & forward_Rt_m & ld_ex) ? Rd_data_memory :
+                     (RegWrite_m & forward_Rt_m) ? Rd_data_mStage :
+                     Rt_data_d;
+
+endmodule
+`default_nettype wire
--- project/demo3/verilog/proc.v
+++ project/demo3/verilog/proc.v
@@ -11,20 +11,115 @@ module proc (/*AUTOARG*/
 
    input wire clk;
    input wire rst;
-
    output reg err;
 
-   // None of the above lines can be modified
+   wire halt;
+   wire nop_d, nop_x, nop_m, nop_wb;
+   wire [15:0] PC, pc_f, pc_d, pc_x, nxtPC_x;
+   wire [15:0] instr_f, instr_d, instr_x, instr_m, instr_wb;
+   wire [15:0] WBdata;
+   wire [15:0] Rs_x, Rt_x, Imm_x, Rs_d, Rt_d, Imm_d, Rt_m;
+   wire [4:0] opcode_d, opcode_x;
+   wire [1:0] func_d, func_x, resultSel_d, resultSel_x;
+   wire halt_d, MemRead_d, MemWrite_d, MemToReg_d, branch_d, savePC_d;
+   wire halt_x, MemRead_x, MemWrite_x, MemToReg_x, branch_x, savePC_x;
 
-   // OR all the err ouputs for every sub-module and assign it as this
-   // err output
-   
-   // As desribed in the homeworks, use the err signal to trap corner
-   // cases that you think are illegal in your statemachines
-   
-   
+   // siganl for branch taken used in fetch
+   wire branchTaken_x, branchTaken_m;
+
+   wire halt_m, MemWrite_m, MemRead_m, MemToReg_m;
+   wire MemToReg_wb;
+   wire [15:0] writeData_x, writeData_m, writeData_wb;
+   wire [15:0] memAddr_x, memAddr_m;
+   wire [15:0] memResult_m, memResult_wb;
+   wire halt_wb;
+   wire stall_d;
+   wire done;
+   wire ex_err, D_err;  
+   wire [2:0] Rd_d, Rd_x, Rd_m, Rd_wb;
+   wire RegWrite_d, RegWrite_x, RegWrite_m, RegWrite_wb;
+
+   // signal for forwarding
+   wire [2:0] RtSel_d, RsSel_d;
+
+   // extra signal for hazard control
+   wire [1:0] resultSel_m;
+
+   // extra signal for forwarding
+   wire forward;
+   wire [15:0] Rs_data_forward, Rt_data_forward;
+
    /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
+   hazardControl h0 (//inputs
+                     .Rd_d(Rd_d), .Rs_d(RsSel_d), .Rt_d(RtSel_d), 
+                     .Rd_x(Rd_x), .Rd_m(Rd_m), 
+                     .ResultSel_x(resultSel_x), .ResultSel_m(resultSel_m), .ResultSel_d(resultSel_d),
+                     .RegWrite_x(RegWrite_x), .RegWrite_m(RegWrite_m),
+                     .opcode_d(opcode_d), .opcode_x(opcode_x),
+                     .Rd_data_x(writeData_x), .Rd_data_mStage(writeData_m), .Rd_data_memory(memResult_m),
+                     .Rs_data_d(Rs_d), .Rt_data_d(Rt_d),
+                     //outputs 
+                     .stall(stall_d), .forward(forward), .Rs_forward(Rs_data_forward), .Rt_forward(Rt_data_forward));
+
+   fetch f0       (  .newPC(nxtPC_x), .INSTR(instr_f), .clk(clk), .rst(rst), .incPC(pc_f), .branchTaken(branchTaken_x), .stall(stall_d));
    
+   if_id ifid0    (  .clk(clk), .rst(rst), .halt_d(halt_d),
+                     .stall_d(stall_d),
+                     .instr_f(instr_f), .pc_f(pc_f), .instr_d(instr_d), .pc_d(pc_d),
+                     .branchTaken(branchTaken_x));
+
+   decode d0      (  .clk(clk), .rst(rst), 
+                     .INSTR_d(instr_d), 
+                     .RegWrite_d(RegWrite_d), .Rd_d(Rd_d),
+                     .RsData(Rs_d), .RtData(Rt_d), .Imm(Imm_d), .OPCODE(opcode_d), .FUNC(func_d),
+                     .Rs(RsSel_d), .Rt(RtSel_d),
+                     .nop(nop_d), 
+                     .MemRead(MemRead_d), .MemWrite(MemWrite_d), .MemToReg(MemToReg_d), .branch(branch_d),
+                     .savePC(savePC_d), .D_err(D_err), .resultSel(resultSel_d),
+                     .WBdata(WBdata), .RegWrite_wb(RegWrite_wb), .Rd_wb(Rd_wb),
+                     .branchTaken_x(branchTaken_x));
+
+   id_ex idex0    (  .clk(clk), .rst(rst), .halt_d(halt_d), .halt_x(halt_x), .branchTaken(branchTaken_x),
+                     .stall(stall_d),
+                     .nop_d(nop_d), .nop_x(nop_x), .Rd_d(Rd_d), .Rd_x(Rd_x), 
+                     .RegWrite_d(RegWrite_d), .RegWrite_x(RegWrite_x),
+                     .RsData_d(Rs_data_forward), .RtData_d(Rt_data_forward), .Imm_d(Imm_d), .opcode_d(opcode_d), .func_d(func_d), .currPC_d(pc_d),
+                     .RsData_x(Rs_x), .RtData_x(Rt_x), .Imm_x(Imm_x), .opcode_x(opcode_x), .func_x(func_x), .currPC_x(pc_x),
+                     .MemRead_d(MemRead_d), .MemWrite_d(MemWrite_d), .MemToReg_d(MemToReg_d), .branch_d(branch_d), .savePC_d(savePC_d), .resultSel_d(resultSel_d),
+                     .MemRead_x(MemRead_x), .MemWrite_x(MemWrite_x), .MemToReg_x(MemToReg_x), .branch_x(branch_x), .savePC_x(savePC_x), .resultSel_x(resultSel_x));
+   execute  iex0  (  .clk(clk), .rst(rst), .nop(nop_x),
+                     .opcode(opcode_x), .Rs(Rs_x), .Rt(Rt_x), .Imm(Imm_x), .currPC(pc_x), .FUNC(func_x), 
+                     .MemRead(MemRead_x), .MemWrite(MemWrite_x), .MemtoReg(MemToReg_x), 
+                     .branch(branch_x), .resultSel(resultSel_x),
+	                  .writeData(writeData_x), .nxtPC(nxtPC_x), .MemAddr(memAddr_x), 
+                     .ex_err(ex_err), 
+                     .branchTaken(branchTaken_x));
+
+   ex_mem iexmem0 (  .clk(clk), .rst(rst), .nop_x(nop_x), .nop_m(nop_m), 
+                     .Rd_x(Rd_x), .Rd_m(Rd_m), .RegWrite_x(RegWrite_x), .RegWrite_m(RegWrite_m),
+                     .Addr_x(memAddr_x), .writeData_x(writeData_x), .halt_x(halt_x), .MemWrite_x(MemWrite_x), 
+                     .MemRead_x(MemRead_x), .Rt_x(Rt_x), .MemToReg_x(MemToReg_x),
+                     .Addr_m(memAddr_m), .WriteData_m(writeData_m), .halt_m(halt_m), .MemWrite_m(MemWrite_m), 
+                     .MemRead_m(MemRead_m), .Rt_m(Rt_m), .MemToReg_m(MemToReg_m), 
+                     .nxtPC_x(nxtPC_x), .nxtPC_m(PC),
+                     .resultSel_x(resultSel_x), .resultSel_m(resultSel_m));
+
+   memory imem0   (  .clk(clk), .rst(rst), .halt(halt_m),
+                     .Addr(memAddr_m), 
+                     .WriteData(Rt_m), 
+                     .MemWrite(MemWrite_m), .MemRead(MemRead_m), .ReadData(memResult_m));
+
+   mem_wb imemwb0 (  .clk(clk), .rst(rst), .nop_m(nop_m), .nop_w(nop_wb), 
+                     .Rd_m(Rd_m), .Rd_wb(Rd_wb), .RegWrite_m(RegWrite_m), .RegWrite_wb(RegWrite_wb),
+                     .MemtoReg_m(MemToReg_m), .exResult_m(writeData_m), .memResult_m(memResult_m),
+	                  .MemtoReg_w(MemToReg_wb), .exResult_w(writeData_wb), .memResult_w(memResult_wb), 
+                     .halt_m(halt_m),
+                     .halt_w(halt_wb));
+
+   wb iwb0        (  .clk(clk), .rst(rst), 
+                     .MemtoReg(MemToReg_wb), .exResult(writeData_wb), .memResult(memResult_wb), 
+                     .wbResult(WBdata));
+
 endmodule // proc
 `default_nettype wire
 // DUMMY LINE FOR REV CONTROL :0:
